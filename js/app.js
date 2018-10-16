fce2a40f9ea5d207d6057dbcce9af86480416a29
wangzuoxu <zuoxu.wang@gmail.com>
2018-10-16 10:47:16 +0800

    Site updated: 2018-10-16 10:47:16

--- js/app.js
+++ js/app.js
@@ -1,11 +1,1 @@
-// 页面 header 处理
-var currentScrollHeight = window.pageYOffset, scrollOffset = 0
-window.onscroll = function(e) {
-  scrollOffset = window.pageYOffset - currentScrollHeight
-  currentScrollHeight = window.pageYOffset
-  if(scrollOffset > 0){ //如果向下浏览大于 100px 则隐藏，否则显示
-    document.querySelector('.site-header').classList.add('header-hidden');
-  }else{
-    document.querySelector('.site-header').classList.remove('header-hidden');
-  }
-}
+var currentScrollHeight=window.pageYOffset,scrollOffset=0;window.onscroll=function(e){scrollOffset=window.pageYOffset-currentScrollHeight,currentScrollHeight=window.pageYOffset,0<scrollOffset?document.querySelector(".site-header").classList.add("header-hidden"):document.querySelector(".site-header").classList.remove("header-hidden")};
